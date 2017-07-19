b49bf4624ba0d7c85e9e8e58c3f1a05395abc7d9
kiok46 <kuldeepbb.grewal@gmail.com>
2017-07-19 15:35:06 +0530

    Add screen and create actions

--- actions/TruckerActions.js
+++ actions/TruckerActions.js
@@ -4,19 +4,37 @@ import {
 	FETCHING_DATA_FAILURE
 } from './types';
 
-import Colors from '../constants/Colors';
-import { INITIAL_STATE } from '../reducers/TruckerReducer';
+import axios from 'axios';
 
 
-export const toggleProfileState = (activate) => {
-	if (activate === true){
-		return {
-			type: FETCHING_DATA,
-			payload: false
-		};
-	}
-	return {
-		type: FETCHING_DATA,
-		payload: true
-	};
+export function getData() {
+  return {
+    type: FETCHING_DATA
+  }
+}
+
+export function getDataSuccess(data) {
+  return {
+    type: FETCHING_DATA_SUCCESS,
+    data,
+  }
+}
+
+export function getDataFailure() {
+  return {
+    type: FETCHING_DATA_FAILURE
+  }
+}
+
+export function fetchData() {
+  return (dispatch) => {
+    dispatch(getData())
+    axios.get("https://gist.githubusercontent.com/bgdavidx/9458ff3ae6054a28e0a636367ff77bbf/raw/990adb44389595174da8fc5ec890045e0db66495/gistfile1.txt")
+      .then((response) => {
+        dispatch(getDataSuccess(response.data))
+      })
+	  .catch((err) => {
+		dispatch(getDataFailure())
+	  })
+  }
 }
--- reducers/TruckerReducer.js
+++ reducers/TruckerReducer.js
@@ -5,15 +5,32 @@ import {
 } from '../actions/types';
 
 export const INITIAL_STATE = {
-	default_value: true,
+	data: [],
+	isFetching: false,
+	error: false
 };
 
 export default ( state=INITIAL_STATE, action ) => {
 	switch (action.type) {
 		case FETCHING_DATA:
-		    return { ...state, default_value: action.payload};
-		default:
-			return state;
-
+	      return {
+	        ...state,
+	        data: [],
+	        isFetching: true
+	      }
+	    case FETCHING_DATA_SUCCESS:
+	      return {
+	        ...state,
+	        isFetching: false,
+	        data: action.data
+	      }
+	    case FETCHING_DATA_FAILURE:
+	      return {
+	        ...state,
+	        isFetching: false,
+	        error: true
+	      }
+	    default:
+	      return state
 	}
 };
